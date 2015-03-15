a0df2c8ea80c7fc9b84eb97b888386a67715c260
杨继孟 <yangfan5157@qq.com>
2015-03-15 09:15:48 +0800

    随机函数有待完善

--- iMidudu/Biz.cs
+++ iMidudu/Biz.cs
@@ -40,7 +40,17 @@ namespace iMidudu
         }
         public static double GenerateRandomAmount(string bouns)
         {
-            return new Random().Next(0, 10) / 10.0;
+            static var d50=6042,d2=604801;//d50是50元红包的数量，d2是总共红包数量
+            var amount=new Random().Next(1,604801);
+            if(amount<=d50){
+                d50=d50-1;
+                d2=d2-1;
+            }
+            else{
+                d2=d2-1;
+            }
+            return amount;
+           // return new Random().Next(0, 10) / 10.0;
         }
 
     }
