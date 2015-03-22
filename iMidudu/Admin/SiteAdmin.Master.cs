b45cad11f1f9d322781f72d24b098632072f2e0a
张波 <zhangbo99@gmail.com>
2015-03-22 10:10:24 +0800

    建立母版页

--- iMidudu/Admin/SiteAdmin.Master.cs
+++ iMidudu/Admin/SiteAdmin.Master.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace iMidudu.Admin
 {
-    public partial class Default : System.Web.UI.Page
+    public partial class SiteAdmin : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
